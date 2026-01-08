37847490b2813be7f032b94dc2a7be12d7474607
rupadevii <rupadevi7094@gmail.com>
2026-01-08 23:06:10 +0530

    Commit-1

---
+++ src/components/SumCalculator.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+const SumCalculator = () => {
+    const [sum, setSum] = useState(0)
+    const [input, setInput] = useState(0)
+    const [nums, setNums] = useState([0])
+
+    function handleChange(e){
+        setInput(e.target.value)
+        let currValue = Number(e.target.value)
+        if(currValue){
+            setNums((prev) => [...prev, currValue])
+        }
+    }
+
+    useEffect(() => {
+        setSum(nums.reduce((acc, ele) => acc+ele), 0)
+    }, [nums])
+
+    return (
+        <div>
+        <input 
+            type="number"
+            onChange={handleChange}
+            value={input} />
+        <p>Sum is {sum}</p>
+        </div>
+    )
+}
+
+export default SumCalculator
